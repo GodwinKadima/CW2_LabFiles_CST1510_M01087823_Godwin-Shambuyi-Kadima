16fcdb974734263f9fb554f6c556224360bab00e
GodwinKadima <GS939@live.mdx.ac.uk>
2025-11-14 10:15:04 +0400

    add auth code

--- week 7/auth.py
+++ week 7/auth.py
@@ -1,3 +1,14 @@
+from pathlib import Path
+import re
 import bcrypt
-import os
 
+USER_DATA = Path('user.txt')
+
+def hash_password(plain_text_password):
+	# Encode the password to bytes, required by bcrypt
+    password_bytes = plain_text_password.encode('utf-8')
+	# Generate a salt and hash the password
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password_bytes, salt)
+	# Decode the hash back to a string to store in a text file
+    return hashed_password
